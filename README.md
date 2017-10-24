ae132d5e35018bb3fdafc725ca029f590b161999
Lucian Sabo <luciansabo@gmail.com>
2017-10-24 10:32:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### SolarBox Monitor
+
+Code for a circuit monitoring temperature, humidity, SLA battery voltage and state of charge.
+A typical usage is to monitor a box containing a solar charge controller and a battery which is kept outside.
+
+Components used:
+- Sparkfun ESP8266 Thing (https://www.sparkfun.com/products/13804)
+- I2C Humidity and temperature sensor: Si7021 (https://www.sparkfun.com/products/13763)
+- voltage divider for measuring battery voltage
---
+++ SolarBoxMonitor.ino
@@ -0,0 +1,444 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Social networks:            http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP8266 chip.
+
+  Note: This requires ESP8266 support package:
+    https://github.com/esp8266/Arduino
+
+  Please be sure to select the right ESP8266 module
+  in the Tools -> Board menu!
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include "SparkFun_Si7021_Breakout_Library.h"
+#include <Wire.h>
+#include <EEPROM.h>
+#include "Config.h"
+
+// --------------------------------------------------------
+
+float humidity;
+float temp;
+float voltage = 0;
+byte chargeLevel = 0;
+
+enum statuses {
+  STATUS_OK,
+  STATUS_NOTICE,
+  STATUS_WARNING,
+  STATUS_DANGER
+} envStatus;
+
+enum batteryStatuses {
+  STATUS_DISCHARGED,
+  STATUS_CHARGED,
+  STATUS_FLOAT,
+  STATUS_BULK,
+  STATUS_BOOST,
+  STATUS_EQUALIZE,
+  STATUS_OVERVOLTAGE
+} batteryStatus;
+
+int prevEnvStatus, prevBatteryStatus;
+
+char statusText[100], batteryStatusText[100];
+
+WidgetLED ledWeather(V2);
+WidgetLED ledBattery(V5);
+
+//Create Instance of SI7021 temp and humidity sensor
+Weather sensor;
+
+//--------------------------------------------------------------
+
+void setup()
+{
+  WiFi.mode(WIFI_OFF);
+  WiFi.forceSleepBegin();
+  // Debug console
+  Serial.begin(9600);
+
+  //Initialize the I2C sensors and ping them
+  sensor.begin();
+  yield();
+  readPreviousParameters();
+  readWeather();
+  readVoltage();
+  calculateParams();
+
+  if (networkSetup()) {
+    sendAlerts();
+    logToBlynk();
+    Blynk.disconnect();
+  }
+
+  goToSleep();
+  //debugInfo();
+}
+
+//--------------------------------------------------------------
+
+void loop()
+{
+
+}
+
+/**
+ * Read parameters saved in EEPROM
+ */
+void readPreviousParameters()
+{
+  unsigned short addr = 0;
+  EEPROM.begin(2);
+  // read saved status
+  prevEnvStatus = EEPROM.read(addr);
+  prevBatteryStatus = EEPROM.read(++addr);
+}
+
+/**
+ * Connect to wifi and blynk using a fixed ip and specific channel/bssid
+ * Will force wake up from sleep
+ * 
+ * @return bool success
+ */
+bool networkSetup()
+{
+  unsigned long t0 = millis();
+  // Disable the WiFi persistence.  The ESP8266 will not load and save WiFi settings in the flash memory.
+  WiFi.persistent( false );
+  WiFi.forceSleepWake();
+  WiFi.mode(WIFI_STA);
+  delay(1);
+  // Bring up the WiFi connection
+  WiFi.config(ip, gateway, subnet, dns);
+  WiFi.begin(ssid, pass, channel, bssid);
+  yield();
+  
+  int counter = 0;
+  while (WiFi.status() != WL_CONNECTED && (counter <= WIFI_RECONNECT_TRIES)) {
+    delay(WIFI_RECONNECT_DELAY);
+    Serial.print(".");
+    counter++;
+  }
+  
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.printf("Could not connect to %s\n", ssid);
+    return false;
+  }
+
+  //Serial.printf("Connected to %s in %d ms\n", ssid, wifiTime);
+
+  Blynk.config(auth);  // in place of Blynk.begin(auth, ssid, pass);
+  while (!Blynk.connect());
+
+  //Serial.printf("Connected to Blynk in %d ms\n", blynkTime);
+
+  return true;
+}
+
+//--------------------------------------------------------------
+/**
+ * Calculates the parameters after reading the sensors
+ * 
+ * - envStatus
+ * - statusText
+ */
+void calculateParams()
+{
+  strcpy(statusText, " ");
+  envStatus = STATUS_OK;
+
+  if (humidity > 96) { // solar controller operates at maximum 96% humidity
+    strcpy(statusText, "EXCES UMIDITATE."); // excessive humidity
+    envStatus = STATUS_DANGER;
+  } else if (humidity > 85) {
+    envStatus = STATUS_WARNING;
+    strcpy(statusText, "Avert. Umiditate."); // humidity warning
+  }
+
+  // battery charge 0 - 40
+  // Discharge: -15 ~ 50°C
+  // 40°C 103%, 25°C 100%, 0°C 86%
+  if (temp < -25) {
+    envStatus = STATUS_DANGER;
+    strcat(statusText, " INGHET.");
+  } else if (temp < 0) {
+    if (envStatus < STATUS_DANGER) {
+      envStatus = STATUS_WARNING;
+    }
+    strcat(statusText, " Risc inghet."); // low temp, freezing
+  } else if (temp < 10) {
+    if (envStatus < STATUS_WARNING) {
+      envStatus = STATUS_NOTICE;
+    }
+    strcat(statusText, " Temp. scazuta."); // low temp notice
+  } else if (temp > 60) {
+    envStatus = STATUS_DANGER;
+    strcat(statusText, " INCENDIU."); // possible fire
+  } else if (temp > 50) {
+    if (envStatus < STATUS_DANGER) {
+      envStatus = STATUS_WARNING;
+    }
+    strcat(statusText, " Temp. f. ridicata."); // very high temp
+  } else if (temp > 37) {
+    if (envStatus < STATUS_WARNING) {
+      envStatus = STATUS_NOTICE;
+    }
+    strcat(statusText, " Temp. ridicata."); // high temp
+  }
+
+  // battery voltage status
+  if (voltage > 15.55) {
+      batteryStatus = STATUS_OVERVOLTAGE;
+      strcpy(batteryStatusText, "Supra-tensiune");
+      strcat(statusText, " SUPRA-TENSIUNE BATERIE."); // over-voltage
+  } else if (voltage > 14.52) {
+      batteryStatus = STATUS_EQUALIZE;
+      strcpy(batteryStatusText, "Incarcare Egalizare");
+  } else if (voltage > 14.3) {
+      batteryStatus = STATUS_BOOST;
+      strcpy(batteryStatusText, "Incarcare Boost");
+  } else if (voltage > 13.7) {
+      batteryStatus = STATUS_FLOAT;
+      strcpy(batteryStatusText, "Incarcare Float");
+  } else if (voltage > 13.2) {
+      batteryStatus = STATUS_BULK;
+      strcpy(batteryStatusText, "Incarcare Bulk");
+  } else if (voltage > 11.9) {
+      byte capacity = 100;
+      if (temp > 25) {
+        capacity += (temp - 25) * 0.2;
+      } else {
+        capacity -= (25 - temp) * 0.55;
+      }
+      sprintf(batteryStatusText, "Se descarca. %d %", round(capacity));
+      batteryStatus = STATUS_CHARGED;
+  } else {
+      batteryStatus = STATUS_DISCHARGED;
+      strcat(statusText, " BATERIE DESCARCATA."); // discharged
+      strcpy(batteryStatusText, "Sub-tensiune");
+  }
+
+  chargeLevel = getSoc(voltage, temp);
+}
+
+/**
+ * Temperature corrected State Of Charge
+ */
+byte getSoc(float voltage, float temp)
+{
+  float voltageMap[] = {11.5, 11.80, 11.9, 12.1, 12.20, 12.4, 12.47, 12.57, 12.67, 12.75, 12.85};
+  float diff;
+  int i;
+
+  if (temp > 27) {
+    voltage += diff / 7.0 * 0.005;
+  } else {
+    diff = 27 - temp;
+    voltage -= (diff / 7.0 * 0.005) + (diff * 0.001);
+  }
+
+  for (i = 0; i <= 10; i++) {
+    if (voltage <= voltageMap[i]) {
+      return i * 10;
+    }
+  }
+
+  return 100;
+}
+
+/**
+ *  Measure Relative Humidity from the Si7021 sensor
+ */
+
+void readWeather()
+{
+  humidity = sensor.getRH();
+  // unconnected sensor give -6
+  if (humidity < 0) {
+    humidity = NULL;
+  }
+
+  // Temperature is measured every time RH is requested.
+  // It is faster, therefore, to read it from previous RH
+  // measurement with getTemp() instead with readTemp()
+  temp = sensor.getTemp();
+
+  if (temp < -45) {
+    temp = NULL;
+  }
+}
+
+/**
+ * Read volage using ADC pin on Sparkfun 8266 Thing
+ * Analog reading goes from 0 - 1023. ADC voltage range is 0 - 1V
+ */
+ void readVoltage()
+ {
+  // read the input on analog pin 0:
+  int adcValue = analogRead(A0);
+  voltage = adcValue * 0.018516157;
+ }
+
+/**
+ * Send alerts using Blynk
+ */
+void sendAlerts()
+{
+  unsigned short addr = 0;
+
+  // no notification if the state is not changed
+  if (prevEnvStatus == envStatus && batteryStatus == prevBatteryStatus) {
+    return;
+  }
+  
+  // todo: send this alert only once if conditions go away but not more than 10 times a day
+  if (envStatus == STATUS_DANGER  || batteryStatus == STATUS_DISCHARGED || batteryStatus == STATUS_OVERVOLTAGE) {
+    Blynk.email("SolarBox Alert", statusText);
+    char buff[50];
+    sprintf(buff, "SolarBox Alert: %s", statusText);
+    Blynk.notify(buff);
+  }
+
+  // write the changed values to the appropriate address of the EEPROM.
+  // these values will remain there when the board is turned off.
+  addr = 0;
+  if (prevEnvStatus != envStatus) {
+    EEPROM.write(addr, envStatus);
+  }
+
+  if (batteryStatus != prevBatteryStatus) {
+    EEPROM.write(++addr, batteryStatus);
+  }
+  EEPROM.commit();
+  yield();
+}
+
+/**
+ * Publish measurements to the Blynk service
+ * 
+ * Virtual Pins:
+ * 
+ * - V0 - `float` temperature rounded to one decimal in range -40-125 C
+ * - V1 - `int` humidity in range 0-100
+ * - V2 - LED widget for environment status
+ * - V3 - `char` weather status text
+ * - V4 -  `float` SLA battery voltage in range 0-16V
+ * - V5 - LED widget for battery status
+ * - V6 - `char` battery status text
+ * - V7 - `byte` battery charge level
+ */
+void logToBlynk()
+{
+  Blynk.run();
+  Blynk.virtualWrite(0, round(temp * 10) / 10.0); // round to one decimal
+  Blynk.virtualWrite(1, round(humidity));
+  
+  switch (envStatus) {
+    case STATUS_NOTICE:
+      ledWeather.setColor(COLOR_YELLOW);
+      break;    
+    case STATUS_WARNING:
+      ledWeather.setColor(COLOR_ORANGE);
+      break;
+    case STATUS_DANGER:
+      ledWeather.setColor(COLOR_RED);
+      break;
+    default:
+      ledWeather.setColor(COLOR_GREEN);
+      
+  }
+  ledWeather.on();
+
+  switch (batteryStatus) {
+    case STATUS_OVERVOLTAGE:
+    case STATUS_DISCHARGED:
+      ledBattery.setColor(COLOR_RED);
+      break;
+    case STATUS_EQUALIZE:
+      ledBattery.setColor(COLOR_ORANGE);
+      break;
+    case STATUS_BOOST:
+      ledBattery.setColor(COLOR_YELLOW);
+      break;
+    case STATUS_FLOAT:
+      ledBattery.setColor(COLOR_BLUE);
+      break;     
+    default:
+      ledBattery.setColor(COLOR_GREEN);
+      
+  }
+  ledBattery.on();
+  
+  Blynk.virtualWrite(3, statusText);
+  Blynk.virtualWrite(4, voltage);
+  Blynk.virtualWrite(6, batteryStatusText);
+  Blynk.virtualWrite(7, chargeLevel);
+}
+
+//---------------------------------------------------------------
+
+void goToSleep()
+{
+  unsigned short seconds;
+  
+  if (envStatus == STATUS_DANGER || batteryStatus == STATUS_OVERVOLTAGE) {
+    // 30 sec sleep if danger
+    seconds = 30;
+  } else if (envStatus != STATUS_DANGER && (voltage > 12.3 && voltage < 13)) {
+    // 3 mins sleep at night if nothing happens
+    seconds = 180;
+  } else {
+    // normal 1 min sleep
+    seconds = 60;
+  }
+  ESP.deepSleep(seconds * 1000000);
+  yield();
+}
+
+//---------------------------------------------------------------
+
+void debugInfo()
+{
+  //This function prints the weather data out to the default Serial Port
+
+  Serial.print("Temp:");
+  Serial.print(temp);
+  Serial.print("C, ");
+
+  Serial.print("Humidity:");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  Serial.print("Status");
+  Serial.println(statusText);
+
+  Serial.print("Voltage:");
+  Serial.println(voltage);
+  Serial.printf("SoC: %d\n", chargeLevel);
+}
+
